94dbbd31c6dd73ba73285298ec8d1abe7d663013
熊玮 <xw0810@163.com>
2017-11-21 20:04:34 +0800

    修改为新版组件形式

--- WheelCurvedPicker.android.js
+++ WheelCurvedPicker.android.js
@@ -1,95 +1,89 @@
 'use strict';
 
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-	View,
-	ColorPropType,
-	requireNativeComponent,
+  View,
+  ColorPropType,
+  requireNativeComponent,
 } from 'react-native';
 
 
-var WheelCurvedPicker = React.createClass ({
+class WheelCurvedPicker extends Component {
 
-	propTypes: {
-		...View.propTypes,
+  static propTypes = {
+    ...View.propTypes,
 
-		data: PropTypes.array,
+    data: PropTypes.array,
 
-		textColor: ColorPropType,
+    textColor: ColorPropType,
 
-		textSize: PropTypes.number,
+    textSize: PropTypes.number,
 
-		itemStyle: PropTypes.object,
+    itemStyle: PropTypes.object,
 
-		itemSpace: PropTypes.number,
+    itemSpace: PropTypes.number,
 
-		onValueChange: PropTypes.func,
+    onValueChange: PropTypes.func,
 
-		selectedValue: PropTypes.any,
+    selectedValue: PropTypes.any,
 
-		selectedIndex: PropTypes.number,
-	},
+    selectedIndex: PropTypes.number,
+  };
 
-	getDefaultProps(): Object {
-		return {
-			itemStyle : {color:"white", fontSize:26},
-			itemSpace: 20,
-		};
-	},
+  static defaultProps = {
+    itemStyle: { color: "white", fontSize: 26 },
+    itemSpace: 20,
+  };
 
-	getInitialState: function() {
-		return this._stateFromProps(this.props);
-	},
+  constructor(props) {
+    super(props);
+    this.state = this._stateFromProps(props);
+  }
 
-	componentWillReceiveProps: function(nextProps) {
-		this.setState(this._stateFromProps(nextProps));
-	},
+  componentWillReceiveProps(nextProps) {
+    this.setState(this._stateFromProps(nextProps));
+  }
 
-	_stateFromProps: function(props) {
-		var selectedIndex = 0;
-		var items = [];
-		React.Children.forEach(props.children, function (child, index) {
-			if (child.props.value === props.selectedValue) {
-				selectedIndex = index;
-			}
-			items.push({value: child.props.value, label: child.props.label});
-		});
+  _stateFromProps = (props) => {
+    var selectedIndex = 0;
+    var items = [];
+    React.Children.forEach(props.children, function (child, index) {
+      if (child.props.value === props.selectedValue) {
+        selectedIndex = index;
+      }
+      items.push({ value: child.props.value, label: child.props.label });
+    });
 
-		var textSize = props.itemStyle.fontSize
-		var textColor = props.itemStyle.color
+    var textSize = props.itemStyle.fontSize;
+    var textColor = props.itemStyle.color;
 
-		return {selectedIndex, items, textSize, textColor};
-	},
+    return { selectedIndex, items, textSize, textColor };
+  };
 
-	_onValueChange: function(e: Event) {
-		if (this.props.onValueChange) {
-			this.props.onValueChange(e.nativeEvent.data);
-		}
-	},
+  _onValueChange = (e) => {
+    if (this.props.onValueChange) {
+      this.props.onValueChange(e.nativeEvent.data);
+    }
+  };
 
-	render() {
-		return <WheelCurvedPickerNative
-				{...this.props}
-				onValueChange={this._onValueChange}
-				data={this.state.items}
-				textColor={this.state.textColor}
-				textSize={this.state.textSize}
-				selectedIndex={parseInt(this.state.selectedIndex)} />;
-	}
-});
+  render() {
+    return <WheelCurvedPickerNative
+      {...this.props}
+      onValueChange={this._onValueChange}
+      data={this.state.items}
+      textColor={this.state.textColor}
+      textSize={this.state.textSize}
+      selectedIndex={parseInt(this.state.selectedIndex)}/>;
+  }
+}
 
-WheelCurvedPicker.Item = React.createClass({
-	propTypes: {
-		value: PropTypes.any, // string or integer basically
-		label: PropTypes.string,
-	},
+WheelCurvedPicker.Item = (props) => null;
 
-	render: function() {
-		// These items don't get rendered directly.
-		return null;
-	},
-});
+WheelCurvedPicker.Item.propTypes = {
+  value: PropTypes.any, // string or integer basically
+  label: PropTypes.string,
+};
 
 var WheelCurvedPickerNative = requireNativeComponent('WheelCurvedPicker', WheelCurvedPicker);
 
