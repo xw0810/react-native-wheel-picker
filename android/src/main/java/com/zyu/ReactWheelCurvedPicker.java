da3b7bcfdcff42f5a1f805436b2fa819e7228a7b
熊玮 <xw0810@163.com>
2017-11-22 10:25:31 +0800

    1、更新wheelpicker新版

--- android/src/main/java/com/zyu/ReactWheelCurvedPicker.java
+++ android/src/main/java/com/zyu/ReactWheelCurvedPicker.java
@@ -1,15 +1,6 @@
 package com.zyu;
 
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.LinearGradient;
-import android.graphics.Paint;
-import android.graphics.Shader;
-import android.os.SystemClock;
-import android.util.AttributeSet;
-
-import com.aigestudio.wheelpicker.core.AbstractWheelPicker;
-import com.aigestudio.wheelpicker.view.WheelCurvedPicker;
+import com.aigestudio.wheelpicker.WheelPicker;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.WritableMap;
@@ -23,7 +14,7 @@ import java.util.List;
 /**
  * @author <a href="mailto:lesliesam@hotmail.com"> Sam Yu </a>
  */
-public class ReactWheelCurvedPicker extends WheelCurvedPicker {
+public class ReactWheelCurvedPicker extends WheelPicker {
 
     private final EventDispatcher mEventDispatcher;
     private List<Integer> mValueData;
@@ -33,14 +24,15 @@ public class ReactWheelCurvedPicker extends WheelCurvedPicker {
         mEventDispatcher = reactContext.getNativeModule(UIManagerModule.class).getEventDispatcher();
         setOnWheelChangeListener(new OnWheelChangeListener() {
             @Override
-            public void onWheelScrolling(float deltaX, float deltaY) {
+            public void onWheelScrolled(int offset) {
+
             }
 
             @Override
-            public void onWheelSelected(int index, String data) {
-                if (mValueData != null && index < mValueData.size()) {
+            public void onWheelSelected(int position) {
+                if (mValueData != null && position < mValueData.size()) {
                     mEventDispatcher.dispatchEvent(
-                            new ItemSelectedEvent(getId(), mValueData.get(index)));
+                            new ItemSelectedEvent(getId(), mValueData.get(position)));
                 }
             }
 
@@ -50,34 +42,10 @@ public class ReactWheelCurvedPicker extends WheelCurvedPicker {
         });
     }
 
-    @Override
-    protected void drawForeground(Canvas canvas) {
-        super.drawForeground(canvas);
-
-        Paint paint = new Paint();
-        paint.setColor(Color.WHITE);
-        int colorFrom = 0x00FFFFFF;//Color.BLACK;
-        int colorTo = Color.WHITE;
-        LinearGradient linearGradientShader = new LinearGradient(rectCurItem.left, rectCurItem.top, rectCurItem.right/2, rectCurItem.top, colorFrom, colorTo, Shader.TileMode.MIRROR);
-        paint.setShader(linearGradientShader);
-        canvas.drawLine(rectCurItem.left, rectCurItem.top, rectCurItem.right, rectCurItem.top, paint);
-        canvas.drawLine(rectCurItem.left, rectCurItem.bottom, rectCurItem.right, rectCurItem.bottom, paint);
-    }
-
-    @Override
-    public void setItemIndex(int index) {
-        super.setItemIndex(index);
-        unitDeltaTotal = 0;
-		mHandler.post(this);
-    }
-
     public void setValueData(List<Integer> data) {
         mValueData = data;
     }
 
-    public int getState() {
-        return state;
-    }
 }
 
 class ItemSelectedEvent extends Event<ItemSelectedEvent> {
